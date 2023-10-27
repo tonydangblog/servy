4e4e8da74b54e187389d627eca26a77b3424e18b
Tony Dang <tony@tonydang.blog>
2023-10-27 16:18:45 -0700

    refactor out file handlers

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -6,6 +6,7 @@ defmodule Servy.Handler do
 
   import Servy.Plugins, only: [rewrite_path: 1, log: 1, track: 1]
   import Servy.Parser, only: [parse: 1]
+  import Servy.FileHandler, only: [handle_file: 2]
 
   @doc "Transform the request into a response."
   def handle(request) do
@@ -71,18 +72,6 @@ defmodule Servy.Handler do
 
   def route(%{path: path} = conv) do
     %{conv | status: 404, resp_body: "No #{path} here!"}
-  end
-
-  def handle_file({:ok, content}, conv) do
-    %{conv | status: 200, resp_body: content}
-  end
-
-  def handle_file({:error, :enoent}, conv) do
-    %{conv | status: 404, resp_body: "File not found"}
-  end
-
-  def handle_file({:error, reason}, conv) do
-    %{conv | status: 500, resp_body: "File error: #{reason}"}
   end
 
   # def route(%{method: "GET", path: "/about"} = conv) do
