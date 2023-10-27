9b34fac668e6581f145fa6fa3d0f4d7abf650b0a
Tony Dang <tony@tonydang.blog>
2023-10-27 13:41:41 -0700

    use regex to match path

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -2,6 +2,7 @@ defmodule Servy.Handler do
   def handle(request) do
     request
     |> parse
+    |> log
     |> rewrite_path
     |> log
     |> route
@@ -21,11 +22,23 @@ defmodule Servy.Handler do
     %{conv | path: "/wildthings"}
   end
 
-  def rewrite_path(%{path: "/bears?id=" <> id} = conv) do
-    %{conv | path: "/bears/#{id}"}
+  # def rewrite_path(%{path: "/bears?id=" <> id} = conv) do
+  #   %{conv | path: "/bears/#{id}"}
+  # end
+
+  def rewrite_path(%{path: path} = conv) do
+    regex = ~r{\/(?<thing>\w+)\?id=(?<id>\d+)}
+    captures = Regex.named_captures(regex, path)
+    rewrite_path_captures(conv, captures)
   end
 
-  def rewrite_path(conv), do: conv
+  def rewrite_path_captures(conv, %{"thing" => thing, "id" => id}) do
+    %{conv | path: "/#{thing}/#{id}"}
+  end
+
+  def rewrite_path_captures(conv, nil), do: conv
+
+  # def rewrite_path(conv), do: conv
 
   def log(conv), do: IO.inspect(conv)
 
