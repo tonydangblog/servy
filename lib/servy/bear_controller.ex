8dba4978b6aad472acc2e0e90589e7f7560e1d7c
Tony Dang <tony@tonydang.blog>
2023-10-30 15:25:07 -0700

    refactor render into it's own module

--- lib/servy/bear_controller.ex
+++ lib/servy/bear_controller.ex
@@ -1,17 +1,7 @@
 defmodule Servy.BearController do
   alias Servy.Bear
   alias Servy.Wildthings
-
-  @templates_path Path.expand("../../templates", __DIR__)
-
-  defp render(conv, template, bindings) do
-    content =
-      @templates_path
-      |> Path.join(template)
-      |> EEx.eval_file(bindings)
-
-    %{conv | status: 200, resp_body: content}
-  end
+  import Servy.View, only: [render: 3]
 
   def index(conv) do
     bears =
