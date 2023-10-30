67efc423c8299abd7735ef59704de92cc6f10141
Tony Dang <tony@tonydang.blog>
2023-10-30 12:52:55 -0700

    Use BearController to handle routes

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -3,6 +3,8 @@ require Logger
 defmodule Servy.Handler do
   @moduledoc "Handles HTTP requests."
   alias Servy.Conv
+  alias Servy.BearController
+
   @pages_path Path.expand("../../pages", __DIR__)
 
   import Servy.Plugins, only: [rewrite_path: 1, log: 1, track: 1]
@@ -27,7 +29,7 @@ defmodule Servy.Handler do
   end
 
   def route(%Conv{method: "GET", path: "/bears"} = conv) do
-    %{conv | status: 200, resp_body: "Teddy, Smokey, Paddington"}
+    BearController.index(conv)
   end
 
   def route(%Conv{method: "GET", path: "/pages/" <> page_name} = conv) do
@@ -57,20 +59,17 @@ defmodule Servy.Handler do
   end
 
   def route(%Conv{method: "GET", path: "/bears/" <> id} = conv) do
-    %{conv | status: 200, resp_body: "Bear #{id}"}
+    params = Map.put(conv.params, "id", id)
+    BearController.show(conv, params)
   end
 
   # name=Baloo&type=Brown
   def route(%Conv{method: "POST", path: "/bears"} = conv) do
-    %{
-      conv
-      | status: 201,
-        resp_body: "Created a #{conv.params["type"]} bear named #{conv.params["name"]}!"
-    }
+    BearController.create(conv, conv.params)
   end
 
   def route(%Conv{method: "DELETE", path: "/bears/" <> _id} = conv) do
-    %{conv | status: 403, resp_body: "Deleting a bear is forbidden!"}
+    BearController.delete(conv, conv.params)
   end
 
   def route(%Conv{method: "GET", path: "/about"} = conv) do
