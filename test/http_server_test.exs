6eda204eb659fdf196718ce6c6d230710270e3b4
Tony Dang <tony@tonydang.blog>
2023-10-31 14:12:50 -0700

    add test for http_server

---
+++ test/http_server_test.exs
@@ -0,0 +1,44 @@
+defmodule HttpServerTest do
+  use ExUnit.Case
+
+  test "test request to server" do
+    spawn(fn -> Servy.HttpServer.start(4321) end)
+
+    request = """
+    GET /bears HTTP/1.1\r
+    Host: example.com\r
+    User-Agent: ExampleBrowser/1.0\r
+    Accept: */*\r
+    \r
+    """
+
+    response = Servy.HttpClient.send_request(request)
+
+    expected_response = """
+    HTTP/1.1 200 OK\r
+    Content-Type: text/html\r
+    Content-Length: 354\r
+    \r
+    <h1>All The Bears!</h1>
+
+    <ul>
+      <li>Brutus - Grizzly</li>
+      <li>Iceman - Polar</li>
+      <li>Kenai - Grizzly</li>
+      <li>Paddington - Brown</li>
+      <li>Roscoe - Panda</li>
+      <li>Rosie - Black</li>
+      <li>Scarface - Grizzly</li>
+      <li>Smokey - Black</li>
+      <li>Snow - Polar</li>
+      <li>Teddy - Brown</li>
+    </ul>
+    """
+
+    assert remove_whitespace(response) == remove_whitespace(expected_response)
+  end
+
+  defp remove_whitespace(text) do
+    String.replace(text, ~r{\s}, "")
+  end
+end
