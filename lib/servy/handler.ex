80f0beec510a9e41d454f151fdc8a2dcc258fb3b
Tony Dang <tony@tonydang.blog>
2023-11-01 12:05:27 -0700

    add template for sensors page

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -10,6 +10,7 @@ defmodule Servy.Handler do
   import Servy.Plugins, only: [rewrite_path: 1, log: 1, track: 1]
   import Servy.Parser, only: [parse: 1]
   import Servy.FileHandler, only: [handle_file: 2]
+  import Servy.View, only: [render: 3]
 
   @doc "Transform the request into a response."
   def handle(request) do
@@ -35,6 +36,7 @@ defmodule Servy.Handler do
     where_is_bigfoot = Task.await(task)
 
     %{conv | status: 200, resp_body: inspect({snapshots, where_is_bigfoot})}
+    render(conv, "sensors.heex", snapshots: snapshots, location: where_is_bigfoot)
   end
 
   def route(%Conv{method: "GET", path: "/kaboom"} = _conv) do
