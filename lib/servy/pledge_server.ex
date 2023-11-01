d4100b710c4f7483b9608612482ba8e2c3afe459
Tony Dang <tony@tonydang.blog>
2023-11-01 14:58:15 -0700

    comment out debug lines

--- lib/servy/pledge_server.ex
+++ lib/servy/pledge_server.ex
@@ -64,20 +64,20 @@ defmodule Servy.PledgeServer do
   end
 end
 
-alias Servy.PledgeServer
+# alias Servy.PledgeServer
 
-pid = PledgeServer.start()
+# pid = PledgeServer.start()
 
-send(pid, {:stop, "hammertime"})
+# send(pid, {:stop, "hammertime"})
 
-IO.inspect(PledgeServer.create_pledge("larry", 10))
-IO.inspect(PledgeServer.create_pledge("moe", 20))
-IO.inspect(PledgeServer.create_pledge("curly", 30))
-IO.inspect(PledgeServer.create_pledge("daisy", 40))
-IO.inspect(PledgeServer.create_pledge("grace", 50))
+# IO.inspect(PledgeServer.create_pledge("larry", 10))
+# IO.inspect(PledgeServer.create_pledge("moe", 20))
+# IO.inspect(PledgeServer.create_pledge("curly", 30))
+# IO.inspect(PledgeServer.create_pledge("daisy", 40))
+# IO.inspect(PledgeServer.create_pledge("grace", 50))
 
-IO.inspect(PledgeServer.recent_pledges(), label: "PledgeServer.recent_pledges(pid)")
+# IO.inspect(PledgeServer.recent_pledges(), label: "PledgeServer.recent_pledges(pid)")
 
-IO.inspect(PledgeServer.total_pledged())
+# IO.inspect(PledgeServer.total_pledged())
 
-IO.inspect(Process.info(pid, :messages))
+# IO.inspect(Process.info(pid, :messages))
