81398b7e7c145b737b66d7c10ebb5993d385079b
Tony Dang <tony@tonydang.blog>
2023-11-02 14:32:05 -0700

    refactor to use GenServer

--- lib/servy/four_oh_four_counter.ex
+++ lib/servy/four_oh_four_counter.ex
@@ -1,4 +1,4 @@
-defmodule Servy.GenericServer do
+defmodule Servy.GenericServerr do
   def start(callback_module, initial_state, name) do
     pid = spawn(__MODULE__, :listen_loop, [initial_state, callback_module])
     Process.register(pid, name)
@@ -39,47 +39,24 @@ defmodule Servy.GenericServer do
 end
 
 defmodule Servy.FourOhFourCounter do
-  alias Servy.GenericServer
+  alias Servy.GenericServerr
   @process_name :four_oh_four_counter_process
 
   def start do
     IO.inspect("Starting the 404 counter process...")
-    GenericServer.start(__MODULE__, %{}, @process_name)
+    GenericServerr.start(__MODULE__, %{}, @process_name)
   end
 
   def bump_count(path) do
-    GenericServer.call(@process_name, {:bump_count, path})
+    GenericServerr.call(@process_name, {:bump_count, path})
   end
 
   def get_count(path) do
-    GenericServer.call(@process_name, {:get_count, path})
+    GenericServerr.call(@process_name, {:get_count, path})
   end
 
   def get_counts() do
-    GenericServer.call(@process_name, :get_counts)
-  end
-
-  # Server
-  def listen_loop(state) do
-    receive do
-      {sender, :bump_count, path} ->
-        new_state = Map.update(state, path, 1, &(&1 + 1))
-        send(sender, {:response, "Bumped #{path}"})
-        listen_loop(new_state)
-
-      {sender, :get_count, path} ->
-        count = Map.get(state, path, 0)
-        send(sender, {:response, count})
-        listen_loop(state)
-
-      {sender, :get_counts} ->
-        send(sender, {:response, state})
-        listen_loop(state)
-
-      unexpected ->
-        IO.puts("Unexpected message: #{inspect(unexpected)}")
-        listen_loop(state)
-    end
+    GenericServerr.call(@process_name, :get_counts)
   end
 
   def handle_call({:bump_count, path}, state) do
