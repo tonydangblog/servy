9152c0b5e91d7b999afd9d1c99f012add452d8bd
Tony Dang <tony@tonydang.blog>
2023-11-03 10:50:28 -0700

    get server pid via client api

--- lib/servy/kick_starter.ex
+++ lib/servy/kick_starter.ex
@@ -1,10 +1,18 @@
 defmodule Servy.KickStarter do
   use GenServer
 
+  # Client interface -----------------------------------------------------------
+
   def start do
     IO.puts("Starting the kickstarter...")
     GenServer.start(__MODULE__, :ok, name: __MODULE__)
   end
+
+  def get_server do
+    GenServer.call(__MODULE__, :get_server)
+  end
+
+  # Server callbacks -----------------------------------------------------------
 
   def init(:ok) do
     Process.flag(:trap_exit, true)
@@ -12,11 +20,17 @@ defmodule Servy.KickStarter do
     {:ok, server_pid}
   end
 
+  def handle_call(:get_server, _from, state) do
+    {:reply, state, state}
+  end
+
   def handle_info({:EXIT, _pid, reason}, _state) do
     IO.puts("HTTP server exited #{inspect(reason)}")
     server_pid = start_server()
     {:noreply, server_pid}
   end
+
+  # Private functions ----------------------------------------------------------
 
   defp start_server do
     IO.puts("Starting the HTTP server...")
